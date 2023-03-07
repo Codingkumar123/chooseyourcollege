4d1c5f3b6a85c9efe5929bdb83a71848481a89d8
Codingkumar123 <83939392+Codingkumar123@users.noreply.github.com>
2023-03-07 21:14:29 +0530

    Add files via upload

---
+++ thanks.js
@@ -0,0 +1,12 @@
+
+
+function openPopup()
+{
+  var element=document.getElementById("popup");
+  element.classList.add("open");
+
+}
+function closePopup()
+{
+  element.classList.remove("open");
+}
